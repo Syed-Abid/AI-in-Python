791a50e78ba70f559397a236806608c32a858710
Syed Abid Hassan <abidhassanjaffri31@gmail.com>
2023-10-28 00:48:38 +0500

    Delete Lab 1d.py

--- Lab 1d.py
+++
@@ -1,8 +0,0 @@
-words = {'hat', 'bat', 'lat', 'fridges', 'watered', 'boat', 'rope', 'lowered'}
-new_words = {}
-for x in words:
-    if len(5) in x:
-        new_words.append(x)
-
-print(new_words)
-
