6d2e0139e989a13c20e7cce998e55cc5a4beab12
Syed Abid Hassan <abidhassanjaffri31@gmail.com>
2023-10-30 14:48:46 +0500

    Add files via upload

---
+++ Lab 2/Lab 2e.py
@@ -0,0 +1,34 @@
+# Program to count from 0 to 100
+#n = 0
+#count = 0
+#for n in range(1):
+#    count = count + 1
+#    for i in range(10):
+#        for j in range(1, 11):
+#            print(i * 10 + j, end=" ")
+#        print()
+
+# Program to make a multiplication table using loop
+#num = int(input("Enter a number you want the table for: "))
+#for n in range(1, 11):
+#    print(num, 'x', n, '=', num * n)
+
+# Program to print the numbers from 1 to 10 backwards
+#a = 11
+#for i in range(0, 10):
+#    a = a - 1
+#    print(a)
+
+# Program to print all even numbers up to 10
+#num1 = 0
+#num2 = 10
+#for num in range(num1, num2 + 1):
+#    if num % 2 == 0:
+#        print(num)
+
+# Program to print the sum of numbers from 100 to 200
+sum = 0
+for i in range(100, 200):
+    sum += i
+print(sum)
+
